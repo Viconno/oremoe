6ee9915d75610593d08bf5060a39731b65cbe4b5
Viconno <viconno@gmail.com>
2014-07-25 15:52:19 +0300

    Oremoe
    My personal website

---
+++ js/hover.js
@@ -0,0 +1,21 @@
+$('#twitter').hover(
+	function() {
+		$('#infotwitter').show();
+	}, function() {
+		$('#infotwitter').hide();
+	}
+);
+$('#mal').hover(
+	function() {
+		$('#infomal').show();
+	}, function() {
+		$('#infomal').hide();
+	}
+);
+$('#github').hover(
+	function() {
+		$('#infogithub').show();
+	}, function() {
+		$('#infogithub').hide();
+	}
+);
